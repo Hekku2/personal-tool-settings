24a25ee56098bf5599f9e09e5ab5ccde7c4f4d12
Heikki-Jussi Niemi <heikki-jussi.niemi@protacon.com>
2018-03-05 14:43:59 +0200

    Powershell/chocolate script for generic dev tools install for Windows environment (.net)

---
+++ dev-tools-install.ps1
@@ -0,0 +1,12 @@
+﻿######################################################
+# This installs development tools for .net development
+# This requires Chocolatey.
+######################################################
+
+cinst git -y
+cinst 7zip.install -y
+cinst googlechrome -y
+cinst dotnet4.5 -y
+cinst dotnetcore-sdk -y
+cinst azure-cli
+cinst notepadplusplus.install
